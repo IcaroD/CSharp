7df085b92ef834fbdcfa2a54086d848faa97a3df
ICARO DAVILA SANTOS <icaro.davila.santos@gmail.com>
2023-07-11 17:43:16 -0300

    Add files via upload

---
+++ Retangulo/Retangulo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        double largura, comprimento, valor, area, precoDometro;
+
+        CultureInfo culture = CultureInfo.InvariantCulture;
+
+        Console.Write("Digite a largura do terreno: ");
+        largura = double.Parse(Console.ReadLine(), culture);
+        Console.Write("Digite o comprimento do terreno: ");
+        comprimento = double.Parse(s: Console.ReadLine(), culture);
+        Console.Write("Digite o valor do metro quadrado: ");
+        valor = double.Parse(s: Console.ReadLine(), culture);
+
+        area = largura * comprimento;
+        precoDometro = valor * area;
+
+        Console.WriteLine("Area do terreno = " + area.ToString("F2", culture));
+        Console.WriteLine("Preco do terreno = " + precoDometro.ToString("F2", culture));
+    }
+}
---
+++ soma_impares/soma_impares/Program.cs
@@ -0,0 +1,29 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int x, y, temp, soma;
+
+        Console.WriteLine("Digite dois numeros:");
+        x = int.Parse(Console.ReadLine());
+        y = int.Parse(Console.ReadLine());
+
+        if (x > y)
+        {
+            temp = x;
+            x = y;
+            y = temp;
+        }
+
+        soma = 0;
+        for (int i = x + 1; i < y; i++)
+        {
+            if (i % 2 != 0)
+            {
+                soma = soma + i;
+            }
+        }
+
+        Console.WriteLine("SOMA = " + soma);
+    }
+}
